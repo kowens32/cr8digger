f77e209f452bbf302b9d29a0aa1ec9f8ecd5dac9
Kimberly <kowens4@elon.edu>
2020-09-24 20:12:14 -0400

    first design

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-
 'use strict'
 
 console.log('here is a hidden message');
-
-let today = new Date();
-let formatDate = today.toDateString();
-let selectElement = document.getElementById('date');
-selectElement.innerHTML = formatDate;
